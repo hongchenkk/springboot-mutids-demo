28c65e59a57170679aecde3de72382b7b9f72ec1
hong.chen <hong.chen@audaque.com>
2021-09-30 10:32:37 +0800

    添加数据库脚本

---
+++ src/main/resources/doc/sql/springboot-mutids-demo-salve.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.24 (64 bit)
+MySQL - 5.5.51 : Database - springboot-mutids-demo-salve
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`springboot-mutids-demo-salve` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `springboot-mutids-demo-salve`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `sex` tinyint(1) DEFAULT NULL COMMENT '性别[1:男,0:女]',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='用户测试表';
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`name`,`age`,`sex`) values (1,'xiaoli_slave',18,1),(2,'chong_slave',28,1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
